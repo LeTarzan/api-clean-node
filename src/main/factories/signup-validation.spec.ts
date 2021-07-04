221b6f9444ad0ccda0d2238fba5771ce68d51552
letarzan <leandrorevolve@gmail.com>
2021-07-04 18:48:42 -0300

    refactor: move EmailValidation to the EmailValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,10 +1,22 @@
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-field-validation'
 import { RequireFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
+import { EmailValidator } from '@/presentation/protocols/email-validator'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
 
 describe('SignUP Validation', () => {
   test('Should call ValidationComposite with all validations', async () => {
@@ -17,6 +29,8 @@ describe('SignUP Validation', () => {
     }
 
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+
+    validations.push(new EmailValidation('email', makeEmailValidator()))
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
---
+++ src/presentation/helpers/validators/email-validation.ts
@@ -0,0 +1,21 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { Validation } from './Validation'
+import { EmailValidator } from '../../protocols/email-validator'
+
+export class EmailValidation implements Validation {
+  private readonly emailValidator: EmailValidator
+  private readonly fieldName: string
+
+  constructor(fieldName: string, emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+    this.fieldName = fieldName
+  }
+
+  validate(input: any): Error {
+    const isValid = this.emailValidator.isValid(input[this.fieldName])
+
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
