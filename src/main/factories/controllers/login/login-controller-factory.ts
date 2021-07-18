46037d6b58c471675090421f6587668f8bae73c3
letarzan <leandrorevolve@gmail.com>
2021-07-18 13:42:01 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -0,0 +1,11 @@
+import { makeLogControllerDecorator } from './../../decorator/log-controller-decorator-factory';
+import { makeDbAuthentication } from './../../usecases/authentication/db-authentication-factory';
+import { makeLoginValidation } from './login-validation-factory';
+import { Controller } from '../../../../presentation/protocols/controller';
+import { LoginController } from '../../../../presentation/controllers/login/login-controller';
+
+export const makeLoginController = (): Controller => {  
+  const loginController = new LoginController(makeDbAuthentication(), makeLoginValidation())
+
+  return makeLogControllerDecorator(loginController)
+}
---
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -0,0 +1,12 @@
+import { DbAddAccount } from './../../../../data/usecases/add-account/db-add-account';
+import { AddAccount } from './../../../../domain/useCases/add-account';
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
+
+export const makeDbAddAccount = (): AddAccount => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository)
+}
