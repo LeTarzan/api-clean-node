f719499022e4a408178c392abd850bee4d76f830
letarzan <leandrorevolve@gmail.com>
2021-07-25 20:01:13 -0300

    feat: ensure DbLoadAccountByToken return nulls if LoadAccountByTokenRepository return null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -80,4 +80,15 @@ describe('DbLoadAccountByToken', () => {
     expect(loadByTokenSpy).toHaveBeenLastCalledWith('any_token', 'any_role')
   })
 
+  test('Shoud return null if LoadAccountByTokenRepository return null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)))
+
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBeNull()
+  })
+
 })
