63a601685acea2634b47e41f07bb814e6f81c2e0
letarzan <leandrorevolve@gmail.com>
2021-07-28 23:11:59 -0300

    feat: ensure LoadSurveyController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,13 @@
+import { HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
+import { Controller } from './../../../protocols/controller'
+
+export class LoadSurveysController implements Controller {
+  constructor (
+    private readonly loadSurveys: LoadSurveys
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
