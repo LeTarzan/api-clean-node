7e1aa103898323edac22c235378786fc1e2d538d
letarzan <leandrorevolve@gmail.com>
2021-08-11 22:56:35 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,16 @@
 import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+
+import {
+  badRequest,
+  serverError,
+  unauthorized
+} from './components'
+
+import {
+  accountSchema,
+  errorSchema,
+  loginParamsSchema
+} from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -8,6 +18,10 @@ export default {
     title: 'Clean node API',
     description: 'API para realizar enquetes',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0',
+    url: 'https://opensource.org/licenses/gpl-license'
   },
   servers: [{
     url: '/api'
@@ -20,6 +34,12 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest: badRequest,
+    serverError: serverError,
+    unauthorized: unauthorized
   }
 }
